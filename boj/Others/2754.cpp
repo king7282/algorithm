d670e0bfb9cacc0cefef549e217745f2df910b90
Yeong Jun Joo <juyj7282@gmail.com>
2021-05-06 08:54:24 +0900

    boj 2754 easy problem

---
+++ boj/Others/2754.cpp
@@ -0,0 +1,18 @@
+#include <map>
+#include <string>
+#include <iostream>
+
+std::map<std::string, double> map =
+{ {"A+", 4.3}, {"A0", 4.0}, {"A-", 3.7},
+  {"B+", 3.3}, {"B0", 3.0}, {"B-", 2.7},
+  {"C+", 2.3}, {"C0", 2.0}, {"C-", 1.7},
+  {"D+", 1.3}, {"D0", 1.0}, {"D-", 0.7},
+  {"F", 0.0} };
+
+int main(void) {
+	std::string input;
+
+	std::cin >> input;
+
+	printf("%.1lf\n", map[input]);
+}
