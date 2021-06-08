9570a4fb7d2ae1e70a4d4ed144f29e8ce1515eb2
Yeong Jun Joo <juyj7282@gmail.com>
2021-06-08 11:42:23 +0900

    boj 14681

---
+++ boj/Others/14681.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    if(a > 0 && b > 0)
+        printf("1\n");
+    else if(a < 0 && b > 0)
+        printf("2\n");
+    else if(a < 0 && b < 0)
+        printf("3\n");
+    else
+        printf("4\n");
+}
